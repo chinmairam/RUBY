b8e88d265cb2ffa9ce6ed0f145ce0be51b712cb5
chinmairam <53008719+chinmairam@users.noreply.github.com>
2019-12-29 13:44:47 +0530

    Add files via upload

---
+++ palindrome.rb
@@ -0,0 +1,19 @@
+def palind
+ rev=0
+ temp=0
+ puts "Enter a number to be reversed"
+ x=gets.chomp.to_i
+ n=x
+ until x==0
+  temp=x%10
+  rev=(rev*10)+temp
+  x=x/10
+ end
+ if n==rev
+  puts "#{n} is a palindrome"
+ else 
+  puts "#{n} is not a palindrome"
+ end
+end
+palind
+
